0d7e73a0bfa0e29a8708a40415d3296bd49aed38
atharv1205 <atharvadudhe125@gmail.com>
2025-09-15 00:58:18 +0530

    title cahnges

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'GatiSutra',
+  description: 'Automatic Traffic Management System',
+  generator: '1.0',
 }
 
 export default function RootLayout({
